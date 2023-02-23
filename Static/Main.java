9278ce6659951006518e2f433047197b5423d813
AldairBP <aldairbp@outlook.es>
2023-02-23 13:28:48 -0600

    The class Main is added as well as the Student class (id static variable)

---
+++ Static/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // As well as we create an object we print their values to show how the id
+        // increases
+        Student s1 = new Student("Natalia", 20);
+        System.out.println(s1);
+        Student s2 = new Student("Nachito", 24);
+        System.out.println(s2);
+        Student s3 = new Student("Maldopavo", 26);
+        System.out.println(s3);
+    }
+}
---
+++ Static/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    String name;
+    int age;
+    static int id = 100; // We declare a static variable that belongs to the class and not to the object
+
+    public Student(String name, int age) {
+        id++; // The variable increases each time we call to the constructor
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [id = " + id + ", name = " + this.name + ", age = " + this.age + "]";
+    }
+
+}
