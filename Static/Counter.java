061f9b52dad8622add2dfe3c539239aa4f9e7920
AldairBP <aldairbp@outlook.es>
2023-02-23 18:19:25 -0600

    Now we add or substract passing the parameter studentsNumber

--- Static/Counter.java
+++ Static/Counter.java
@@ -1,18 +1,27 @@
+import javax.lang.model.util.ElementScanner14;
+
 // In this program is shown how the static methods works
 // In this case we simulate a counter for the students that are in the school
 public class Counter {
-    static int count;
 
-    public Counter() {
-        System.out.println("The number of students in the school is = ");
+    int studentsNumber;
+    static int studentsCount;
+
+    public static String add(int studentsNumber) {
+        studentsCount = studentsCount + studentsNumber;
+        return "The number of students in the school is = " + studentsCount;
     }
 
-    public static int add() {
-        return count++;
-    }
+    public static String substract(int studentsNumber) {
+        // We check if there are enoght students to substact the quantity provided
+        if (studentsCount - studentsNumber < 0) {
+            System.out.println("There is no more students to substract");
+            return "The number of students at the school is = " + studentsCount;
+        } else {
+            studentsCount = studentsCount - studentsNumber;
+            return "The number of students at the school is = " + studentsCount;
+        }
 
-    public static int substract() {
-        return count--;
     }
 
 }
